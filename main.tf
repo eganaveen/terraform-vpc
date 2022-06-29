c9fb98e867ab592dc7bc0c5ca2510fbaee2543ad
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-29 11:15:59 +0530

    deleted all vpc and related subnets, route table

--- main.tf
+++ main.tf
@@ -2,9 +2,4 @@ module "vpc" {
   source = "./vendor/modules/vpc"
   VPC_CIDR = var.VPC_CIDR
   ENV = var.ENV
-  SUBNET_CIDR = var.SUBNET_CIDR
-  AZ = var.AZ
-  DEFAULT_VPC_ID = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT = var.DEFAULT_VPC_RT
 }
