8fee9e1a5fceadb1a9c4c0553031ff54dbb7ecfd
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-30 12:38:43 +0530

    route table

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ module "vpc" {
   source = "./vendor/modules/vpc"
   VPC_CIDR = var.VPC_CIDR
   ENV = var.ENV
-  SUBNET_CIDR = var.SUBNET_CIDR
+  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
   AZ = var.AZ
   DEFAULT_VPC_ID = var.DEFAULT_VPC_ID
   DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
